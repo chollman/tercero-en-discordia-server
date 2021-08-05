4a17515c74991771efb944768f7a610dbfd60122
Claudio Hollman <hollman@strands.com>
2021-08-04 21:22:19 -0300

    Express server initiated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 require("dotenv").config();
 
 app.get("/", (req, res) => {
-    res.send("hello from node ");
+    res.send("hello from node again");
 });
 
 const port = process.env.PORT || 8000;
