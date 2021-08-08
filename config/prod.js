1e33736898e6b3cafcfad2962efc4909dfdf7abc
Claudio Hollman <hollman@strands.com>
2021-08-08 12:32:01 -0300

    Changed env variables

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    //googleClientID: "240802540696-o6r7jp3ketpt44hss6ch2kl2he7gtk9i.apps.googleusercontent.com",
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    //googleClientSecret: "Sl_m_xU_5dnQvfKKggjl1TnG",
+    mongoURI: process.env.MONGO_URI,
+    // mongoURI:
+    //     "mongodb+srv://chollman:t11hGI9n3i3Gc55N@tercero-en-discordia-pr.gwznm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    cookieKey: process.env.COOKIE_KEY,
+};
