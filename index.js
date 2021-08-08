da5dc6303b33325b33d83b9aa01191fd8f6b9abb
Claudio Hollman <hollman@strands.com>
2021-08-08 11:57:32 -0300

    Adding Cookies

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("./services/passport");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
+require("./models/user");
+require("./services/passport");
 
 mongoose.connect(keys.mongoURI, {
     useNewUrlParser: true,
@@ -10,6 +13,16 @@ mongoose.connect(keys.mongoURI, {
 });
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000, //30 days in miliseconds
+        keys: [keys.cookieKey],
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
