e2d89ab3b091f0e435e4f582c821754d406dcda3
Gonzalo Bonora <bonora@strands.com>
2021-08-12 01:15:29 -0300

    Improve book model

--- models/book.js
+++ models/book.js
@@ -2,11 +2,29 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const bookSchema = new Schema({
-    id: Number,
-    title: String,
-    description: String,
-    coverImage: String,
-    backCoverImage: String
+    title: {
+        type: String,
+        required: true,
+        maxlength: 100,
+    },
+    author: {
+        type: String,
+        required: true,
+        maxlength: 100,
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 2000,
+    },
+    coverImage: {
+        data: Buffer,
+        contentType: String,
+    },
+    backCoverImage: {
+        data: Buffer,
+        contentType: String,
+    },
 });
 
 module.exports = mongoose.model("books", bookSchema);
