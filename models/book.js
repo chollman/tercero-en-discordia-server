095ef440a1b4dec50d3eee61f377b1f330f2c240
Claudio Hollman <hollman@strands.com>
2021-08-15 17:40:12 -0300

    Added Books timestamps

--- models/book.js
+++ models/book.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const bookSchema = new Schema({
-    id: Number,
-    title: String,
-    description: String,
-    author: String,
-    coverImage: String,
-    backCoverImage: String,
-});
+const bookSchema = new Schema(
+    {
+        id: Number,
+        title: String,
+        description: String,
+        author: String,
+        coverImage: String,
+        backCoverImage: String,
+    },
+    { timestamps: true }
+);
 
 module.exports = mongoose.model("books", bookSchema);
