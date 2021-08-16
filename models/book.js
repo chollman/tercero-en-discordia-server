6241e4b06e1dce611f08e5aec9bae05676969ba5
Claudio Hollman <hollman@strands.com>
2021-08-16 03:33:45 -0300

    Changed book model

--- models/book.js
+++ models/book.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
-const { Schema } = mongoose;
+const { Schema, ObjectId } = mongoose;
 
 const bookSchema = new Schema(
     {
         id: Number,
-        title: String,
-        description: String,
-        author: String,
-        coverImage: String,
-        backCoverImage: String,
+        title: { type: String, trim: true, required: true },
+        description: { type: String, trim: true },
+        author: { type: String, trim: true },
+        category: { type: ObjectId, ref: "Category", required: true },
+        coverImage: { data: Buffer, contentType: String },
+        backCoverImage: { data: Buffer, contentType: String },
     },
     { timestamps: true }
 );
