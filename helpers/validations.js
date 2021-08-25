7667ae45d40eb9f2268c5839321e8bfa7d5ec622
Gonzalo Bonora <gebonora@gmail.com>
2021-08-25 01:45:02 -0300

    Move logic to validations

--- helpers/validations.js
+++ helpers/validations.js
@@ -25,6 +25,21 @@ exports.validateSimpleRequest = (req, res, next) => {
     next();
 }
 
+exports.validateUserSignup = (req, res, next) => {
+    req.check("email")
+        .matches(/.+\@.+\..+/)
+        .withMessage("El mail debe estar bien formado");
+    req.check("password", "Debe escribir una contraseña").notEmpty();
+    req.check("password").isLength({ min: 6 }).withMessage("La contraseña tiene que tener al menos 6 caracteres");
+    const errors = req.validationErrors();
+    if (errors) {
+        const firstError = errors.map((error) => error.msg)[0];
+        return res.status(400).json({ error: firstError });
+    }
+    next();
+};
+
+
 exports.validateFieldsNotNull = (arrayOfFields, errorMsg) => (req, res, next) => {
     if (!arrayOfFields.every((elem) => !!req.fields[elem])) {
         return res.status(400).json({
