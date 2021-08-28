6c0e80316ce0c71180a0c08aad43eb6cfb4ab168
Gonzalo Bonora <gebonora@gmail.com>
2021-08-28 00:26:31 -0300

    Add title to blogpost schema

--- src/models/blogPost.js
+++ src/models/blogPost.js
@@ -3,11 +3,12 @@ const { Schema, ObjectId } = mongoose;
 
 const blogPostSchema = new Schema(
     {
+        title: { type: String, trim: true, required: true, unique: true },
         content: { type: String, trim: true, required: true },
         categories: [{ type: ObjectId, ref: "blog-categories" }],
         tags: [{ type: ObjectId, ref: "blog-tags" }],
         author: { type: String, trim: true },
-        commentaries: [{ type: String, trim: true }],
+        comments: [{ type: String, trim: true }],
     },
     { timestamps: true }
 );
