ba9e073de69018a93fa576ecc4080fb89b08b16c
Gonzalo Bonora <gebonora@gmail.com>
2021-10-12 01:55:44 -0300

    Add testing data

--- config/mongoose/mongooseSetupTest.js
+++ config/mongoose/mongooseSetupTest.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const { MongoMemoryServer } = require("mongodb-memory-server");
+const Category = require("../../src/models/category");
+const BlogCategory = require("../../src/models/blogCategory");
+const Author = require("../../src/models/author");
 
 let mongoServer;
 
@@ -17,6 +20,40 @@ exports.mongooseConnect = async () => {
 
 exports.mongooseDisconnect = async () => {
     await mongoose.connection.close();
-    console.log(mongoServer);
     await mongoServer.stop();
+};
+
+exports.populateDB = async () => {
+    const categories = [{ name: "Action" }, { name: "War" }, { name: "Drama" }, { name: "Terror" }];
+    const blogCategories = [{ name: "Personal" }, { name: "Finance" }, { name: "Editing" }, { name: "Bestsellers" }];
+    const authors = [
+        {
+            name: "Willy",
+            biography: "Some bio",
+            photo: null,
+            twitterLink: "www.twitter.com",
+            instagramLink: "www.ig.com",
+            facebookLink: "www.fb.com",
+        },
+        {
+            name: "John Wayne",
+            biography: "Some bio",
+            photo: null,
+            twitterLink: "www.twitter.com",
+            instagramLink: "www.ig.com",
+            facebookLink: "www.fb.com",
+        },
+    ];
+    for (let category of categories) {
+        let dbCategory = new Category(category);
+        await dbCategory.save();
+    }
+    for (let category of blogCategories) {
+        let dbCategory = new BlogCategory(category);
+        await dbCategory.save();
+    }
+    for (let author of authors) {
+        let dbAuthor = new Author(author);
+        await dbAuthor.save();
+    }
 };
